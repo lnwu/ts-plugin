139f8c4e710a04f7197daf8b7cd271c6b9b617a9
lnwu <wind2729@gmail.com>
2020-12-22 00:44:54 +0800

    test import antd

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,43 +1,10 @@
 import test from "ava";
 
-import { transform } from "./index";
+import compile from "./index";
 
 test("can transform dynamic import code", (t) => {
-  const src = `
-  import { useState, useEffect }, React from 'react'
+  const src = `import { Button } from "antd";`;
+  const out = `import Button from "antd/lib/button";\n`;
 
-  export function AsyncLogin() {
-    const [Login, setLogin] = useState()
-    const Login = useEffect(() => {
-      import('./login').then(({ LoginComponent }) => {
-        setLogin(() => LoginComponent)
-      })
-    }, [])
-
-    if (!Login) {
-      return <Loading />
-    }
-    return <Login />
-  }
-  `;
-  const out = `
-  import { useState, useEffect }, React from 'react'
-  import * as __SOME_UNIQUE_NAME__ from './login'
-
-  export function AsyncLogin() {
-    const [Login, setLogin] = useState()
-    const Login = useEffect(() => {
-      Promise.resolve(__SOME_UNIQUE_NAME__).then(({ LoginComponent }) => {
-        setLogin(() => LoginComponent)
-      })
-    }, [])
-
-    if (!Login) {
-      return <Loading />
-    }
-    return <Login />
-  }
-  `;
-
-  t.is(transform(src), out);
+  t.is(compile(src), out);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,70 @@
-export const transform = (src: string): string => {
-  return "test";
+import { create } from "ts-node";
+import ts, { factory } from "typescript";
+
+const transformer = (
+  ctx: ts.TransformationContext
+): ts.Transformer<ts.SourceFile> => {
+  const visitor: ts.Visitor = (node: ts.Node): ts.Node => {
+    if (ts.isSourceFile(node)) {
+      return ts.visitEachChild(node, visitor, ctx);
+    }
+
+    if (ts.isImportDeclaration(node)) {
+      return updateImportNode(node, ctx);
+    }
+
+    return node;
+  };
+
+  return (sf: ts.SourceFile) => {
+    return ts.visitNode(sf, visitor);
+  };
 };
+
+const updateImportNode = (
+  node: ts.Node,
+  ctx: ts.TransformationContext
+): ts.Node => {
+  let identifierName: string;
+
+  const visitor: ts.Visitor = (node) => {
+    if (ts.isNamedImports(node)) {
+      identifierName = node.getChildAt(1).getText();
+      return ts.factory.createIdentifier(identifierName);
+    }
+
+    if (ts.isStringLiteral(node)) {
+      const libName = node.text;
+      if (identifierName) {
+        const fileName = identifierName.toLocaleLowerCase();
+        return ts.factory.createStringLiteral(`${libName}/lib/${fileName}`);
+      }
+    }
+
+    if (node.getChildCount()) {
+      return ts.visitEachChild(node, visitor, ctx);
+    }
+    return node;
+  };
+
+  return ts.visitEachChild(node, visitor, ctx);
+};
+
+const compile = (sourceCode: string): string => {
+  const source = ts.createSourceFile(
+    "",
+    sourceCode,
+    ts.ScriptTarget.Latest,
+    true
+  );
+
+  const result = ts.transform(source, [transformer]);
+
+  const transformedSourceFile = result.transformed[0];
+  const printer = ts.createPrinter();
+  const resultCode = printer.printFile(transformedSourceFile);
+
+  return resultCode;
+};
+
+export default compile;
